6e75034453eb41876f07965827405b0b8993b9cd
Jaysennn <jaysenstephanus_2327250042@mhs.mdp.ac.id>
2024-10-10 15:19:15 +0700

    connect

---
+++ pab_10okt/bin/pab_10okt.dart
@@ -0,0 +1,5 @@
+import 'package:pab_10okt/pab_10okt.dart' as pab_10okt;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pab_10okt.calculate()}!');
+}
---
+++ pab_10okt/test/pab_10okt_test.dart
@@ -0,0 +1,8 @@
+import 'package:pab_10okt/pab_10okt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
